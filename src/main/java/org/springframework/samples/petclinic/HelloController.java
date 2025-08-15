38a15031c69608449317ac0efcbcfe75c7744365
sairam-reddy2832 <sairam.reddy2832@gmail.com>
2025-08-15 14:20:07 -0500

    Added HelloController with /hello endpoint

---
+++ src/main/java/org/springframework/samples/petclinic/HelloController.java
@@ -0,0 +1,13 @@
+package org.springframework.samples.petclinic;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "{\"message\": \"Hello TD Bank!\"}";
+	}
+}
